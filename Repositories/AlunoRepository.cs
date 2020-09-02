e0a28c7897a1c7882398dde6db22faa112df9779
unknown <gmandelinha@gmail.com>
2020-09-02 16:19:35 -0300

    ADO.NET

--- Repositories/AlunoRepository.cs
+++ Repositories/AlunoRepository.cs
@@ -1,7 +1,9 @@
-﻿using APIBoletim.Domains;
+﻿using APIBoletim.Context;
+using APIBoletim.Domains;
 using APIBoletim.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +11,9 @@ namespace APIBoletim.Repositories
 {
     public class AlunoRepository : IAluno
     {
+        BoletimContext conexao = new BoletimContext();
+
+        SqlCommand cmd = new SqlCommand();
         public Aluno Alterar(Aluno a)
         {
             throw new NotImplementedException();
@@ -16,22 +21,83 @@ namespace APIBoletim.Repositories
 
         public Aluno BuscarPorId(int id)
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "SELECT * FROM Aluno WHERE IdAluno = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            Aluno a = new Aluno();
+
+            while (dados.Read())
+            {
+                a.IdAluno = Convert.ToInt32(dados.GetValue(0));
+                a.Nome = dados.GetValue(1).ToString();
+                a.RA = dados.GetValue(2).ToString();
+                a.Idade = Int32.Parse(dados.GetValue(3).ToString());
+            }
+
+            conexao.Desconectar();
+
+            return a;
         }
 
         public Aluno Cadastrar(Aluno a)
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "INSERT INTO Aluno (Nome, RA, Idade) " +
+                "VALUES" +
+                "(@nome, @ra, @idade)";
+            cmd.Parameters.AddWithValue("@nome", a.Nome);
+            cmd.Parameters.AddWithValue("@ra", a.RA);
+            cmd.Parameters.AddWithValue("@idade", a.Idade);
+
+            cmd.ExecuteNonQuery();
+
+            return a;
         }
 
         public Aluno Excluir(Aluno a)
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "DELETE FROM Aluno WHERE IdAluno = @id";
+
+            if (cmd.Parameters.Contains(a))
+                cmd.Parameters.Remove(a);
+
+            return a;
         }
 
         public List<Aluno> ListarTodos()
         {
-            throw new NotImplementedException();
+
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "SELECT * FROM Aluno";
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            List<Aluno> alunos = new List<Aluno>();
+
+            while (dados.Read())
+            {
+                alunos.Add(
+                    new Aluno()
+                    {
+                        IdAluno = Convert.ToInt32(dados.GetValue(0)),
+                        Nome = dados.GetValue(1).ToString(),
+                        RA = dados.GetValue(2).ToString(),
+                        Idade = Int32.Parse(dados.GetValue(3).ToString())
+                    }
+                );
+            }
+
+            conexao.Desconectar();
+
+            return alunos;
         }
     }
 }
